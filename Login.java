753eae360653636c686dd2896038a8bedd1fb346
Ribo <interstander@gmail.com>
2020-06-28 21:43:02 +0900

    4th commit

---
+++ Login.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Login {
+
+	public static void main(String[] args) {
+		System.out.println("10.로그인 기능을 구현하세요");
+		//저장된 아이디 : 'green'
+		//저장된 비밀번호: 'gr1234'
+
+		String id = "green";
+		String pwd = "gr1234";
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("아이디를 입력하세요 : ");
+		String in_id = in.nextLine();
+		System.out.print("비밀번호를 입력하세요.");
+		String in_pwd = in.nextLine();
+
+		if(in_id.equals(id)) {
+			if(in_pwd.equals(pwd)) {
+				System.out.println("로그인 성공");
+			}else {
+				System.out.println("비밀번호가 틀렸습니다.");
+			}
+		}else {
+			System.out.println("아이디가 틀렸습니다.");
+		}
+	}
+
+}
--- Sum100.java
+++ Sum100.java
@@ -2,11 +2,11 @@ public class Sum100 {
 
 	public static void main(String[] args) {
 		System.out.println("1. 1부터 100까지 합을 구하세요.");
-		int sum =0;
+		int hap =0;
 		for(int i=1;i<=100;i++) {
-			sum+=i;
+			hap+=i;
 		}
-		System.out.println("합계 : "+sum);
+		System.out.println("합계 : "+hap);
 		System.out.println();
 	}
 
