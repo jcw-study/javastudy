5b2234f74d2ee460974e0117d8a0f02f2cf7c571
jcw-study <interstander@gmail.com>
2020-07-02 10:08:21 +0900

    scope-commit

---
+++ Scope.java
@@ -0,0 +1,15 @@
+public class Scope {
+
+	public static void main(String[] args) {
+		int num1 = 3;
+		int num2 = 5;
+		int result = adder(num1,num2);
+		System.out.println("합계 : "+result);
+	}
+	private static int adder(int num1, int num2) {
+		int result;
+		result = num1 + num2;
+		return result;
+	}
+
+}
