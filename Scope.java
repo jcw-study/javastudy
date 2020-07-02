7eb8b9b6fd41edbac52123349030c4118dfc7458
jcw-study <interstander@gmail.com>
2020-07-02 10:47:04 +0900

    Scope-change2

--- Scope.java
+++ Scope.java
@@ -3,13 +3,13 @@ public class git{
 	public static void main(String[] args) {
 		int num1 = 3;
 		int num2 = 5;
-		int result = adder(num1,num2);
-		System.out.println("합계 : "+result);
+		int hap = adder(num1,num2);
+		System.out.println("합계 : "+hap);
 	}
 	private static int adder(int num1, int num2) {
-		int result;
-		result = num1 + num2;
-		return result;
+		int hap;
+		hap = num1 + num2;
+		return hap;
 	}
 
 }
